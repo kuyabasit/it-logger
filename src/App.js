825d56727b5ec229b37be7d4927f393f7d38484c
Abdulbasit Gamoranao <basitgamor@gmail.com>
2021-10-31 17:24:49 +0800

    tech list modal

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddBtn from './components/layouts/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -23,6 +24,7 @@ const App = () => {
         <AddLogModal />
         <EditLogModal />
         <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </>
